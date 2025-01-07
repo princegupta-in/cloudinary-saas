679b81e01a5527460b7560aaab58fb50789d386c
princegupta-in <zprincegupta@gmail.com>
2025-01-07 17:44:12 +0530

    worked on routes

---
+++ app/(app)/social-page/page.tsx
@@ -0,0 +1,9 @@
+
+export default function SocialPage(){
+    
+    return (
+        <div>
+            SocialPage
+        </div>
+    )
+}
---
+++ app/(app)/video-upload/page.tsx
@@ -0,0 +1,9 @@
+
+export default function videoPage(){
+    
+    return (
+        <div>
+            videoPage
+        </div>
+    )
+}
